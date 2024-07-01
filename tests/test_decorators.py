a430c9f81a425fb1353d4ecd4d1e52b3a85f55ba
Ivan Sokolskiy <gladtycoon@gmail.com>
2024-07-01 08:24:55 +0200

    all fixes

--- tests/test_decorators.py
+++ tests/test_decorators.py
@@ -1,21 +1,21 @@
-import pytest
+#import pytest                            # для теста с выбросом исключения
 
-from src.decorators import my_function_1, my_function_2
-
-# from src.decorators import my_function, log       # для тестирования декоратора, выполненного по ТЗ
+from src.decorators import my_function    # для тестирования декоратора, выполненного по ТЗ
 
 
-def test_log_to_logfile():
-    assert my_function_1(2, 1) == 3   # Положительный исход
+def test_log():
+
+    assert my_function(2, 1) == 3   # Положительный исход
 
 
-def test_log_to_console():
-    with pytest.raises(Exception):            # Выбрасывает исключение
-        my_function_2(2, 0)
+# def test_log_to_exception():
+#
+#     with pytest.raises(Exception):       # Выбрасывает исключение
+#         my_function()
 
 
-def test_log_to_console(capsys):              # Перехват сообщения в консоль (если логфайла нет)
+def test_log_to_console(capsys):           # Перехват сообщения в консоль (если логфайла нет)
 
-    my_function_2(2, 1)
+    my_function(2, 1)
     captured = capsys.readouterr()
-    assert captured.out == "my_function ok\n"
+    assert captured.out == "my_function ok\n\n"
