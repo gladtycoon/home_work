54bd4d67ad20ce5d59cdf08a585e0d8590d27f65
Ivan Sokolskiy <Gladtycoon@gmail.com>
2026-01-03 05:18:44 +0200

    fixes modul main.py

--- main.py
+++ main.py
@@ -45,7 +45,7 @@ def main():
             transaction = get_func(path_to_file)
             break
 
-    print(len(transaction))
+    # print(len(transaction))
 
     while True:
         print("Введите статус, по которому необходимо выполнить фильтрацию.\n"
@@ -53,7 +53,7 @@ def main():
         input_status: str = input().upper()
         if input_status in status:
             transaction = filter_by_state(transaction, input_status)
-            print(f'Операции отфильтрованы по статусу "{input_status}", {len(transaction)}')
+            print(f'Операции отфильтрованы по статусу "{input_status}"')
             break
         else:
             print(f'Статус операции "{input_status}" недоступен.')
@@ -64,44 +64,47 @@ def main():
         print('Отсортировать по возрастанию или по убыванию?')
         input_type_of_sorting: bool = input().lower() == 'по убыванию'
         transaction = sort_by_date(transaction, input_type_of_sorting)
-        print(len(transaction))
+        # print(len(transaction))
 
     print('Выводить только рублевые транзакции? Да/Нет')
     user_input: bool = input().lower() == 'да'
     if user_input:
         transaction = list(filter_by_currency(transaction, 'RUB'))
-        print(f"Нашлось рублевых: {len(transaction)}")
 
     print('Отфильтровать список транзакций по определенному слову в описании? Да/Нет?')
-    if user_input:
+    search_choice = input().lower() == 'да'
+    if search_choice:
         print('Введите слово для фильтрации:')
         user_word: str = input().lower()
         transaction = process_bank_search(transaction, user_word)
 
-    print('Распечатываю итоговый список транзакций...')
-    print(f'Всего банковских операций в выборке: {len(transaction)}')
+    if len(transaction) == 0:
+        print(f'Не найдено ни одной транзакции, подходящей под ваши условия фильтрации')
+    else:
+        print('Распечатываю итоговый список транзакций...')
+        print(f'Всего банковских операций в выборке: {len(transaction)}')
 
-    for trans in transaction:
-        state = trans.get('state')
-        date = trans.get('date')
-        amount = trans.get('amount')
-        currency_name = trans.get('currency_name')
-        currency_code = trans.get('currency_code')
-        from_account = trans.get('from') if isinstance(trans.get('from'), str) else None
-        to_account = trans.get('to')
-        description = trans.get('description')
-
-
-        out_print = f'{date[:10]}.{date[5:7]}.{date[:4]} {description}'
-        check_to = get_mask_card_number(to_account) if to_account else "Нет данных"
-        if from_account:
-            check_from = ' -> ' + get_mask_card_number(from_account)
-        else:
-            check_from = ''  # не выводим стрелку если нет FROM
+        for trans in transaction:
+            state = trans.get('state')
+            date = trans.get('date')
+            amount = trans.get('amount')
+            currency_name = trans.get('currency_name')
+            currency_code = trans.get('currency_code')
+            from_account = trans.get('from') if isinstance(trans.get('from'), str) else None
+            to_account = trans.get('to')
+            description = trans.get('description')
 
 
-        summ_print = f'Сумма: {amount} {currency_code}'
-        print(f'{out_print}\n{check_to}{check_from}\n{summ_print}')
+            out_print = f'{date[:10]}.{date[5:7]}.{date[:4]} {description}'
+            check_to = get_mask_card_number(to_account) if to_account else "Нет данных"
+            if from_account:
+                check_from = ' -> ' + get_mask_card_number(from_account)
+            else:
+                check_from = ''  # не выводим стрелку если нет FROM
+
+
+            summ_print = f'Сумма: {amount} {currency_code}'
+            print(f'{out_print}\n{check_to}{check_from}\n{summ_print}')
 
 
 if __name__ == '__main__':
